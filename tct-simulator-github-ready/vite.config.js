cba127bda49ada0893e1f1f4b25879005d183994
Chisato-Tct <c-ando@tech-comms.jp>
2025-05-24 21:05:31 +0900

    Update vite.config.js

--- tct-simulator-github-ready/vite.config.js
+++ tct-simulator-github-ready/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/act-simulator-2.0/',
+  plugins: [react()],
 });
